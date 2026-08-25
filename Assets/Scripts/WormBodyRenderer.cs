5ec4666b2a2a728bd49b718eb7cc6671ac47c8ce
foCs <focs.cc.1987@gmail.com>
2026-08-25 18:02:08 +0200

    Rendered snake with 3D meshes
    Segment scale also works with meshes now

--- Assets/Scripts/WormBodyRenderer.cs
+++ Assets/Scripts/WormBodyRenderer.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using NaughtyAttributes;
 using System.Collections.Generic;
 using System;
+using System.Linq;
+using UnityEngine.Splines;
 
 [ExecuteAlways]
 public class WormBodyRenderer : MonoBehaviour
@@ -15,17 +17,23 @@ public class WormBodyRenderer : MonoBehaviour
     [SerializeField, OnValueChanged(nameof(RecomputeBody))] private float segmentDistance;
     [SerializeField] private float maxSegmentAngle;
 
-    [SerializeField] private float moveSpeed;
+    [Header("Meshes")] 
+    [SerializeField] private Transform meshParent;
+    [SerializeField] private GameObject headMesh;
+    [SerializeField] private GameObject bodyMesh;
+    [SerializeField] private GameObject tailMesh;
 
     [Header("Debug")]
     [SerializeField] private bool debugMode;
     [SerializeField] private Color segmentColor;
 
     [SerializeField] private List<BodySegment> _segments = new();
+    private Vector3 _previousFramePos;
 
     void OnEnable()
     {
         RecomputeBody();
+        ReassignSegmentRadiuses();
     }
 
     void RecomputeBody()
@@ -33,6 +41,8 @@ public class WormBodyRenderer : MonoBehaviour
         if (!simulate) return;
         
         _segments.Clear();
+        
+        // Remove overhead or add segment radiuses 
         while (segmentRadiuses.Count != segmentCount)
         {
             if (segmentRadiuses.Count < segmentCount)
@@ -40,6 +50,7 @@ public class WormBodyRenderer : MonoBehaviour
             else segmentRadiuses.Remove(segmentRadiuses[^1]);
         }
         
+        // Modify segment attributes
         for (int i = 0; i < segmentCount; i++)
         {
             BodySegment newSegment = new BodySegment();
@@ -50,7 +61,32 @@ public class WormBodyRenderer : MonoBehaviour
             newSegment.pos = new Vector3(-i * segmentDistance, 0) + transform.localPosition;
             newSegment.radius = segmentRadiuses[i];
         }
+
+        ReconstructMeshes();
+
+
         //Debug.Log("recomputed body");
+    }
+
+    void ReconstructMeshes()
+    {
+        // Destroy previous meshes (even in editor)
+        var tempList = transform.Cast<Transform>().ToList();
+        foreach(var child in tempList)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+        
+        // Spawn new meshes
+        for (int i = 0; i < segmentCount; i++)
+        {
+            GameObject meshToInstantiate = i == 0 ? headMesh : i == segmentCount - 1 ? tailMesh: bodyMesh;
+            GameObject newMesh = Instantiate(meshToInstantiate, transform);
+            newMesh.name = i == 0 ? $"WormHead_{i}" : i == segmentCount - 1 ? $"WormTail_{i}" : $"WormBody_{i}";
+                
+            newMesh.transform.position = _segments[i].pos;
+            _segments[i].mesh = newMesh;
+        }
     }
 
     void ReassignSegmentRadiuses()
@@ -58,6 +94,7 @@ public class WormBodyRenderer : MonoBehaviour
         for (int i = 0; i < segmentCount; i++)
         {
             _segments[i].radius = segmentRadiuses[i];
+            _segments[i].mesh.transform.localScale = new Vector3(segmentRadiuses[i], segmentRadiuses[i], .5f);
         }
     }
 
@@ -65,12 +102,25 @@ public class WormBodyRenderer : MonoBehaviour
     void Update()
     {
         if (!simulate) return;
+        _previousFramePos = _segments[0].pos;
+        
         if (_segments[0] == null)
             Debug.LogError($"worm body named {gameObject.name} has no segments");
         
-        // Make head follow object position
-        _segments[0].pos = transform.position;
+        MoveHead();
         MoveBody();
+    }
+
+    void MoveHead()
+    {
+        _segments[0].pos = _segments[0].mesh.transform.position;
+        
+        SplineAnimate splineAnim = GetComponent<SplineAnimate>();
+        float time = splineAnim.NormalizedTime;
+        Vector3 tangent = splineAnim.Container.Splines[0].EvaluateTangent(time);
+        
+        float headAngle = Vector3.SignedAngle(Vector3.right, tangent, Vector3.up);
+        _segments[0].mesh.transform.localEulerAngles = new Vector3(0, headAngle + 90, 0);
     }
 
     void MoveBody()
@@ -78,6 +128,7 @@ public class WormBodyRenderer : MonoBehaviour
         // Circle constraint each segment
         for (int i = 1; i < segmentCount; i++)
         {
+            Vector3 oldPos = _segments[i].pos;
             Vector3 posDiff = _segments[i].pos - _segments[i - 1].pos;
             Vector3 newPos = _segments[i - 1].pos + posDiff.normalized * segmentDistance;
             
@@ -101,6 +152,12 @@ public class WormBodyRenderer : MonoBehaviour
             }
             
             _segments[i].pos = newPos;
+            _segments[i].mesh.transform.position = newPos;
+            
+            // Rotate segment mesh
+            Vector3 deltaPos = newPos - oldPos;
+            float meshAngle = Vector3.SignedAngle(Vector3.right, deltaPos, Vector3.up);
+            _segments[i].mesh.transform.localEulerAngles = new Vector3(0,  meshAngle + 90, 0);
         }
     }
 
@@ -120,6 +177,9 @@ public class WormBodyRenderer : MonoBehaviour
 public class BodySegment
 {
     public Vector3 pos;
-    public BodySegment parentSegment;
     public float radius;
+    public GameObject mesh;
+    
+    public BodySegment parentSegment;
+    
 }
